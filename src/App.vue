902ad383799c2ac691e8e72cee99560eaa57a712
4Akr7 <4Akr7@pm.me>
2021-06-02 00:22:29 +0800

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
     <v-main>
       <Main />
     </v-main>
+
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>CMingWang</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
