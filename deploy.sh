8c874e3206a48890a693d6ad1131f6a7a2c5e41a
4Akr7 <4Akr7@pm.me>
2021-06-02 17:31:37 +0800

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:CMingWang/CMingWang.github.io.git main
+git push -f git@github.com:CMingWang/CMingWang.github.io.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
