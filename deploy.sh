2382616006f49bd42d1966c118d5437b8c9e3a71
4Akr7 <4Akr7@pm.me>
2021-06-02 18:19:33 +0800

    Deploy Config

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,9 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
+git config user.name 4Akr7
+git config user.email 4Akr7@pm.me
 git add -A
 git commit -m 'deploy'
 
